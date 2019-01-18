bfbf81ae4bb178e25568cfaaaaa2af2a4e9866e4
Katie Macke <katie.macke@joinroot.com>
2019-01-18 01:53:21 -0500

    add activity status

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ const handleMemberJoin = require('./events/member-join');
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', (member) => handleMemberJoin(member, config.welcome));
+client.on('ready', () => client.user.setActivity(config.activity.name, { type: config.activity.type }));
 
 client.login(config.token);
